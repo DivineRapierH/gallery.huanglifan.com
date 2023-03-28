7ac24bc388230a58dd86a3cb17b3379ff7bd134b
Lifan Huang <lifan_huang@outlook.com>
2023-03-28 13:59:02 +0800

    remove email

--- src/consts.js
+++ src/consts.js
@@ -1,5 +1,4 @@
 export const SITE_TITLE = `Lifan's Blog`;
 export const SITE_DESCRIPTION = 'STILL DEMO';
-export const SITE_EMAIL = 'lifan_huang@outlook.com'
 export const SITE_NAME = 'blog.huanglifan.com';
 export const SITE_URL = "https://blog.huanglifan.com";
